acb6e97529459e045982a61a1334ce8350672720
Aman Kumar <ak1276054@gmail.com>
2026-09-22 23:08:56 +0530

    Update docs with clean universal relative asset paths

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/sih-project/',
+  base: './',
   plugins: [
     react(),
     tailwindcss()
@@ -12,6 +12,6 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    allowedHosts: true
+    open: true
   }
 })
